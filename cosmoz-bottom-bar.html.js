f20c15e9ef8777ffd57608f461e388298cde267c
Patrik Kullman <patrik.kullman@neovici.se>
2020-04-14 10:16:56 +0200

    ci: upgrade repo (#71)
    * ci: upgrade repo
    
    - codeclimate
      - drop eslint plugin
      - drop eslint-provided checks
    - karma problem matcher
    - setup-node
      - eslint problem matcher
      - v12.x
    - update .vscode settings
    - drop bower-stuff from .gitignore
    - drop github-actions-eslint-annotator
    - drop old unused repo files
      - analysis.json
      - CONTRIBUTING.md
      - polymer.json
      - wct.conf.json
      - index.html
      - .jslintrc
    - add es-dev-server config
    - add karma config
    - convert tests to karma
    - upgrade deps
    - move template to own file
    
    Signed-off-by: Patrik Kullman <patrik.kullman@neovici.se>
    
    * fix: lint error fix, demos, deps
    
    - Fix unused Polymer lint error
    - Upgrade deps again
    - drop Polymer iron-test/-demo deps
    - Fix bottom-bar demos
    
    Signed-off-by: Patrik Kullman <patrik.kullman@neovici.se>

---
+++ cosmoz-bottom-bar.html.js
@@ -0,0 +1,134 @@
+import '@webcomponents/shadycss/entrypoints/apply-shim';
+
+import '@polymer/iron-icons';
+import '@polymer/paper-icon-button';
+import '@polymer/paper-menu-button';
+import '@polymer/paper-listbox';
+import '@polymer/iron-selector/iron-selector';
+
+import { html } from '@polymer/polymer/lib/utils/html-tag';
+export default html`
+    <style>
+        :host {
+            display: block;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            max-width: 100%; /* Firefox fix */
+            background-color: inherit;
+            transition: transform 0.5s ease;
+            z-index: 10;
+        }
+
+        [hidden] {
+            display: none !important;
+        }
+
+        #bar {
+            @apply --layout-horizontal;
+            @apply --layout-center;
+            padding: 0 3%;
+        }
+
+        #info {
+            @apply --layout-center;
+            @apply --layout-horizontal;
+            min-width: 5px;
+            padding-right: 3%;
+        }
+
+        #dropdownButton {
+            color: var(--cosmoz-bottom-bar-menubutton-color, var(--light-primary-color));
+            background-color: var(--cosmoz-bottom-bar-menubutton-background-color, var(--dark-primary-color));
+            @apply --cosmoz-bottom-bar-menubutton;
+        }
+
+        #extraToolbarSlot ::slotted(:not(slot)) {
+            @apply --cosmoz-bottom-bar-extra-toolbar-item;
+        }
+
+        #flex {
+            @apply --layout-flex;
+        }
+
+        #toolbar, #extraToolbarSlot {
+            @apply --layout-center;
+            @apply --layout-horizontal;
+            overflow: hidden;
+        }
+
+        #menu {
+            @apply --layout-center;
+            @apply --layout-horizontal;
+        }
+
+        #info,
+        #menu > div {
+            white-space: nowrap;
+        }
+
+        #toolbar ::slotted(*) {
+            margin: 0 0.29em;
+            min-width: 40px;
+            min-height: 40px;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            @apply --cosmoz-bottom-bar-toolbar-item;
+        }
+
+        #toolbar ::slotted([disabled]) {
+            @apply --cosmoz-bottom-bar-toolbar-item-disabled;
+        }
+
+        #toolbar ::slotted([hidden]) {
+            display: none !important;
+        }
+
+        #dropdown {
+            padding: 0;
+        }
+
+        /** Seems like a shady dom scoping/slotting issue */
+        #listboxSizer {
+            max-height: 0;
+            padding: 0 !important;
+        }
+
+        #dropdown ::slotted(*) {
+            display: flex;
+            @apply --layout-center;
+            @apply --layout-horizontal;
+            position: relative;
+            @apply --cosmoz-bottom-bar-menu-item;
+        }
+    </style>
+
+    <div id="bar" style$="[[ _getHeightStyle(computedBarHeight) ]]">
+        <div id="info">
+            <slot name="info"></slot>
+        </div>
+        <div id="flex"></div>
+        <iron-selector id="toolbar" selected-class="[[ selectedClass ]]" on-iron-select="_onActionSelected">
+            <slot id="bottomBarToolbar" name="bottom-bar-toolbar"></slot>
+        </iron-selector>
+        <paper-menu-button id="menu" hidden$="[[ !hasMenuItems ]]" no-animations
+            vertical-offset="[[ barHeight ]]" vertical-align="bottom" horizontal-align="right">
+
+            <paper-icon-button id="dropdownButton" class="dropdown-trigger" slot="dropdown-trigger" icon="menu" toggles raised>
+            </paper-icon-button>
+            <paper-listbox id="dropdown" class="dropdown-content" slot="dropdown-content"
+                selected-class="[[ selectedClass ]]" on-iron-select="_onActionSelected">
+                    <span id="listboxSizer"></span>
+                    <slot id="bottomBarMenu" name="bottom-bar-menu"></slot>
+            </paper-listbox>
+        </paper-menu-button>
+        <iron-selector id="extraToolbarSlot" on-iron-select="_onActionSelected">
+            <slot name="extra" id="extraSlot"></slot>
+        </iron-selector>
+    </div>
+
+    <div hidden style="display:none">
+        <slot id="content"></slot>
+    </div>
+`;
--- cosmoz-bottom-bar.js
+++ cosmoz-bottom-bar.js
@@ -1,20 +1,13 @@
 /* eslint-disable max-lines */
-import '@webcomponents/shadycss/entrypoints/apply-shim';
-
-import '@polymer/iron-icons';
-import '@polymer/paper-icon-button';
-import '@polymer/paper-menu-button';
-import '@polymer/paper-listbox';
-import '@polymer/iron-selector/iron-selector';
-
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag';
 import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
 import { timeOut } from '@polymer/polymer/lib/utils/async';
 import { enqueueDebouncer } from '@polymer/polymer/lib/utils/flush';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { IronResizableBehavior } from '@polymer/iron-resizable-behavior';
+
+import template from './cosmoz-bottom-bar.html.js';
 
 const
 	BOTTOM_BAR_TOOLBAR_SLOT = 'bottom-bar-toolbar',
@@ -42,134 +35,7 @@ const
 	*/
 class CosmozBottomBar extends mixinBehaviors([IronResizableBehavior], PolymerElement) {
 	static get template() { // eslint-disable-line max-lines-per-function
-		return html`
-		<style>
-			:host {
-				display: block;
-				position: absolute;
-				bottom: 0;
-				left: 0;
-				width: 100%;
-				max-width: 100%; /* Firefox fix */
-				background-color: inherit;
-				transition: transform 0.5s ease;
-				z-index: 10;
-			}
-
-			[hidden] {
-				display: none !important;
-			}
-
-			#bar {
-				@apply --layout-horizontal;
-				@apply --layout-center;
-				padding: 0 3%;
-			}
-
-			#info {
-				@apply --layout-center;
-				@apply --layout-horizontal;
-				min-width: 5px;
-				padding-right: 3%;
-			}
-
-			#dropdownButton {
-				color: var(--cosmoz-bottom-bar-menubutton-color, var(--light-primary-color));
-				background-color: var(--cosmoz-bottom-bar-menubutton-background-color, var(--dark-primary-color));
-				@apply --cosmoz-bottom-bar-menubutton;
-			}
-
-			#extraToolbarSlot ::slotted(:not(slot)) {
-				@apply --cosmoz-bottom-bar-extra-toolbar-item;
-			}
-
-			#flex {
-				@apply --layout-flex;
-			}
-
-			#toolbar, #extraToolbarSlot {
-				@apply --layout-center;
-				@apply --layout-horizontal;
-				overflow: hidden;
-			}
-
-			#menu {
-				@apply --layout-center;
-				@apply --layout-horizontal;
-			}
-
-			#info,
-			#menu > div {
-				white-space: nowrap;
-			}
-
-			#toolbar ::slotted(*) {
-				margin: 0 0.29em;
-				min-width: 40px;
-				min-height: 40px;
-				text-overflow: ellipsis;
-				white-space: nowrap;
-				@apply --cosmoz-bottom-bar-toolbar-item;
-			}
-
-			#toolbar ::slotted([disabled]) {
-				@apply --cosmoz-bottom-bar-toolbar-item-disabled;
-			}
-
-			#toolbar ::slotted([hidden]) {
-				display: none !important;
-			}
-
-			#dropdown {
-				padding: 0;
-			}
-
-			/** Seems like a shady dom scoping/slotting issue */
-			#listboxSizer {
-				max-height: 0;
-				padding: 0 !important;
-			}
-
-			#dropdown ::slotted(*) {
-				display: flex;
-				@apply --layout-center;
-				@apply --layout-horizontal;
-				position: relative;
-				@apply --cosmoz-bottom-bar-menu-item;
-			}
-		</style>
-
-		<div id="bar" style$="[[ _getHeightStyle(computedBarHeight) ]]">
-			<div id="info">
-				<slot name="info"></slot>
-			</div>
-			<div id="flex"></div>
-			<iron-selector id="toolbar" selected-class="[[ selectedClass ]]" on-iron-select="_onActionSelected">
-				<slot id="bottomBarToolbar" name="bottom-bar-toolbar"></slot>
-			</iron-selector>
-			<paper-menu-button id="menu" hidden$="[[ !hasMenuItems ]]" no-animations
-				vertical-offset="[[ barHeight ]]" vertical-align="bottom" horizontal-align="right">
-
-				<paper-icon-button id="dropdownButton" class="dropdown-trigger" slot="dropdown-trigger" icon="menu" toggles raised>
-				</paper-icon-button>
-				<paper-listbox id="dropdown" class="dropdown-content" slot="dropdown-content" selected-class="[[ selectedClass ]]" on-iron-select="_onActionSelected">
-						<span id="listboxSizer"></span>
-						<slot id="bottomBarMenu" name="bottom-bar-menu"></slot>
-				</paper-listbox>
-			</paper-menu-button>
-			<iron-selector id="extraToolbarSlot" on-iron-select="_onActionSelected">
-				<slot name="extra" id="extraSlot"></slot>
-			</iron-selector>
-		</div>
-
-		<div hidden style="display:none">
-			<slot id="content"></slot>
-		</div>
-`;
-	}
-
-	static get is() {
-		return 'cosmoz-bottom-bar';
+		return template;
 	}
 
 	// eslint-disable-next-line max-lines-per-function
@@ -562,4 +428,4 @@ class CosmozBottomBar extends mixinBehaviors([IronResizableBehavior], PolymerEle
 	}
 }
 
-customElements.define(CosmozBottomBar.is, CosmozBottomBar);
+customElements.define('cosmoz-bottom-bar', CosmozBottomBar);
